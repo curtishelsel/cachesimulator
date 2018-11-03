bf6483eb5b4aa359adbc6489086307726ac2d6b7
Curtis Helsel <curtisdhelsel@knights.ucf.edu>
2018-11-03 11:26:02 -0400

    Add CacheSimulator file

---
+++ CacheSimulator.java
@@ -0,0 +1,15 @@
+/*	Curtis Helsel
+	EEl4768 - Fall 2018
+	November 3, 2018
+
+	This program simulates a cache using the least recently used
+	and first in first out replacement policies.
+*/
+
+public class CacheSimulator{
+
+
+	public static void main(String[] args){
+	
+	}
+}
