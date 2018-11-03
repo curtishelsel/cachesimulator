5a7fa7e043cbae5708003c9964ddf61bca16818d
Curtis Helsel <curtisdhelsel@knights.ucf.edu>
2018-11-03 11:33:06 -0400

    Add MemoryAccess file

---
+++ MemoryAccess.java
@@ -0,0 +1,17 @@
+public class MemoryAccess{
+	
+	private String operationType;
+	private String byteAddress;
+
+	public MemoryAccess(){
+	
+	}
+
+	public String getOperationType(){
+		return operationType;
+	}
+
+	public String getByteAddress(){
+		return byteAddress;
+	}
+}
